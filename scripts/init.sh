00649e106332ad8cd7d017a0525db9c9f6c87017
lcmps <lucas1498campos@gmail.com>
2022-03-25 18:32:53 -0300

    added OS flag to go build

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CGO_ENABLED=0 go build -ldflags="-s -w" -o exodialib-core -v .
+CGO_ENABLED=0 GOOS=linux go build -ldflags="-s -w" -o exodialib-core -v .
 docker build -t exodialib-core:latest -f Dockerfile .
 docker-compose down
 docker compose -f docker-compose.yaml up --remove-orphans
